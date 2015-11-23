87eaf59a584c6640460616e0449451c2130595f5
quy-ld <leuducquy@gmail.com>
2015-11-23 20:39:34 +0700

    Initial Commit

---
+++ calculatehieghtTextview/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  calculatehieghtTextview
+//
+//  Created by quy on 11/23/15.
+//  Copyright © 2015 ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
